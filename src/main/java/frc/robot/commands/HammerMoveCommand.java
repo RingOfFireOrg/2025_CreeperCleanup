2a1d9f126aa7ae24c36cb7924242a7a067b6111e
puneet38917 <puneetqcsd@gmail.com>
2025-08-31 11:34:25 -0400

    added hammer IO system command

---
+++ src/main/java/frc/robot/commands/HammerMoveCommand.java
@@ -0,0 +1,26 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.HammerSubsystem;
+import java.util.function.DoubleSupplier;
+
+public class HammerMoveCommand extends Command {
+    private final HammerSubsystem hammer;
+    private final DoubleSupplier voltageSupplier;
+
+    public HammerMoveCommand(HammerSubsystem hammer, DoubleSupplier voltageSupplier) {
+        this.hammer = hammer;
+        this.voltageSupplier = voltageSupplier;
+        addRequirements(hammer);
+    }
+
+    @Override
+    public void execute() {
+        hammer.runVoltage(voltageSupplier.getAsDouble());
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        hammer.runVoltage(0.0);
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/HammerIO.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems.interfaces;
+
+//import org.littletonrobotics.junction.AutoLog;
+
+public interface HammerIO {
+    //@AutoLog
+    public static class HammerIOInputs {
+        public double positionDeg = 0.0;
+        public double velocityDegPerSec = 0.0;
+        public double appliedVolts = 0.0;
+        public double currentAmps = 0.0;
+    }
+
+    /** Updates the set of loggable inputs. */
+    public default void updateInputs(HammerIOInputs inputs) {}
+
+    /** Run the hammer at the specified voltage. */
+    public default void setVoltage(double volts) {}
+}
