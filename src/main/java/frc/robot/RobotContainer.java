d2547945e93dc097348d0c36de3d76735001cca0
gnarl <43803+gnarl@users.noreply.github.com>
2025-09-02 21:28:30 -0400

    Added NeoPixel color support

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,70 +20,99 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.XboxController;
-//import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-//import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.commands.HammerMoveCommand;
-//import frc.robot.commands.HammerSwingForwardCommand;
-//import frc.robot.commands.HammerSwingBackwardCommand;
 import frc.robot.commands.TankDriveCommand;
 import frc.robot.subsystems.DriveTrainSubSystem;
 import frc.robot.subsystems.HammerInterfaceReal;
 import frc.robot.subsystems.HammerSubsystem;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
     private final XboxController driverController = new XboxController(0);
-    //private final CommandXboxController ManipulatorController = new CommandXboxController(Constants.CONTROLLER_MANIPULATOR_ID);
+    // private final CommandXboxController ManipulatorController = new
+    // CommandXboxController(Constants.CONTROLLER_MANIPULATOR_ID);
     private final XboxController ManipulatorController = new XboxController(Constants.CONTROLLER_MANIPULATOR_ID);
 
     private final DriveTrainSubSystem driveTrain = new DriveTrainSubSystem();
-    private final HammerSubsystem m_hammer = new HammerSubsystem(new HammerInterfaceReal(Constants.HammerConstants.kMotorPWMPort));
+    private final HammerSubsystem m_hammer = new HammerSubsystem(
+            new HammerInterfaceReal(Constants.HammerConstants.kMotorPWMPort));
 
+    private enum NeoPixelColors {
+        BLUE,
+        RED,
+        GREEN
+    }
 
-    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    private final DigitalOutput dioPin0 = new DigitalOutput(0);
+    private final DigitalOutput dioPin1 = new DigitalOutput(1);
+
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
     public RobotContainer() {
-        configureBindings();  
+        setNeoPixelColor(NeoPixelColors.GREEN);
+        configureBindings();
         driveTrain.setDefaultCommand(
-            new TankDriveCommand(driveTrain, driverController)
-        );
-        
+                new TankDriveCommand(driveTrain, driverController));
     }
 
-    /*private void configureBindings() {
-    }
-    */
-     
-     private void configureBindings() {
+    private void configureBindings() {
         // Example: Run hammer with joystick
         m_hammer.setDefaultCommand(
-            new HammerMoveCommand(
-                m_hammer,
-                () -> {
-                    double xAxis = ManipulatorController.getLeftTriggerAxis() - ManipulatorController.getRightTriggerAxis(); // {0to1} - {0to1} = {-1to1}
-                    return xAxis * Constants.HammerConstants.kMaxVoltage;
-                }
-            )
-        );
-     }
-        //ManipulatorController.a().whileTrue(new HammerSwingForwardCommand(m_hammer));
-        //ManipulatorController.b().whileTrue(new HammerSwingBackwardCommand(m_hammer));
-  
-/*        private void configureBindings() {
-            m_hammer.setDefaultCommand(
-                new RunCommand(
-                    () -> m_hammer.setSpeed(-ManipulatorController.getLeftY())  // negate to make up=forward
-                    )
-            );
-
+                new HammerMoveCommand(
+                        m_hammer,
+                        () -> {
+                            double xAxis = ManipulatorController.getLeftTriggerAxis()
+                                    - ManipulatorController.getRightTriggerAxis(); // {0to1} - {0to1} = {-1to1}
+                            return xAxis * Constants.HammerConstants.kMaxVoltage;
+                        }));
     }
-  */ 
+
+    /** Set color for NeoPixels */
+    private void setNeoPixelColor(NeoPixelColors color) {
+
+        switch (color) {
+            case BLUE:
+                dioPin0.set(false);
+                dioPin1.set(true);
+                break;
+            case RED:
+                dioPin0.set(true);
+                dioPin1.set(false);
+                break;
+            case GREEN:
+                dioPin0.set(true);
+                dioPin1.set(true);
+                break;
+        }
+    }
+
+    // ManipulatorController.a().whileTrue(new HammerSwingForwardCommand(m_hammer));
+    // ManipulatorController.b().whileTrue(new
+    // HammerSwingBackwardCommand(m_hammer));
+
+    /*
+     * private void configureBindings() {
+     * m_hammer.setDefaultCommand(
+     * new RunCommand(
+     * () -> m_hammer.setSpeed(-ManipulatorController.getLeftY()) // negate to make
+     * up=forward
+     * )
+     * );
+     * 
+     * }
+     */
     public double GetDriverRawAxis(int axis) {
         return driverController.getRawAxis(axis);
     }
@@ -98,8 +127,8 @@ public class RobotContainer {
     }
 
     public Command getAutonomousCommand() {
-    return null;
-}
+        return null;
+    }
 
     /**
      * Use this to pass the autonomous command to the main {@link Robot} class.
