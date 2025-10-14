22d9b88839c548f1625443e168742cd3f1df216f
Aubrey <pinknfiddy@gmail.com>
2025-10-14 19:34:06 -0400

    update 2.0 hammer

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -46,11 +46,11 @@ public final class Constants {
 
     // Hammer constants
     public static final class HammerConstants {
-        public static final int kMotorPWMPort = 20; // Robot's PWM port on RoboRio
+        public static final int kMotorPWMPort = 5; // Robot's PWM port on RoboRio
         public static final double kMaxVoltage = 12.0;
         public static final double MAX_SPEED = 0.5;
         public static final double MIN_SPEED = -0.5;
-        public static final int HammerCANID = 21; // CAN ID for the hammer motor controller
+        public static final int HammerCANID = 20; // CAN ID for the hammer motor controller
         public static final double VoltageFactor = 0.5; // Adjust this factor based on testing
     }
 
