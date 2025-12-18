19c77cfed8b2d2a25b1cbfb9c00ef7943c6ea8db
Saurya <coolsaurya2010@gmail.com>
2025-12-18 15:12:45 -0500

    removed unnecessary imports

--- src/main/java/frc/robot/commands/AutoDriveCommand.java
+++ src/main/java/frc/robot/commands/AutoDriveCommand.java
@@ -23,11 +23,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.subsystems.DriveTrainSubSystem;
-import frc.robot.Constants;
-import frc.robot.Constants.DriveConstants;
 
 public class AutoDriveCommand extends Command {
   private final DriveTrainSubSystem driveTrain;
